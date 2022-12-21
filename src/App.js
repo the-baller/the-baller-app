48f4a98fc7c24c86521b0b63dd968a61a013a71d
ishayasolo <ishaya808@gmail.com>
2022-12-21 19:27:41 +0100

    setup request hook

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
+import { useEffect } from 'react';
 import { Route, Routes } from "react-router-dom";
+import useFetch from './hooks/useFetch';
 import Dashboard from "./pages/Dashboard";
 import Home from "./pages/Home";
 import SignIn from "./pages/SignIn";
 
 const App = () => {
+  const { makeRequest, data } = useFetch();
+
+  const callEndpoint = () => {
+    const body = {
+      request_class: 'FetchPayazaAccountBeneficiaryRequest',
+    };
+    // const body = {
+    //   request_class: 'AccountNameEnquiryMerchant',
+    //   bank_code: '000013',
+    //   account_number: '0149378999',
+    // };
+
+    makeRequest(body);
+  };
+
+  useEffect(() => {
+    callEndpoint();
+  }, []);
+
+  useEffect(() => {
+    data && console.log(data);
+  }, [data]);
+
   return (
     <>
       <Routes>
@@ -13,6 +38,6 @@ const App = () => {
       </Routes>
     </>
   );
-}
+};
 
 export default App;
